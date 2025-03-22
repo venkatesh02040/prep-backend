ec5874b7b3894f8181f7a2eb70e6a5c86255688b
venkatesh02040 <ysvh.pandu@gmail.com>
2025-03-22 14:14:18 +0530

    first commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) return res.status(401).json({ message: "Access Denied" });
-
+  const token = req.header("Authorization")?.split(" ")[1];
+if (!token) return res.status(401).json({ message: "Access Denied" });
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
