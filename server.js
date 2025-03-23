5075a06e3ded618e383ec68acf26149504307cf1
venkatesh02040 <ysvh.pandu@gmail.com>
2025-03-24 03:10:34 +0530

    final commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://prepxpert.netlify.app", credentials: true }));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
