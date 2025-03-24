7b86ee429564f95a23bcca245565b86c4524ac9b
venkatesh02040 <ysvh.pandu@gmail.com>
2025-03-24 16:50:49 +0530

    final commit

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -53,24 +53,3 @@ exports.login = async (req, res) => {
       res.status(500).json({ message: "Server error" });
   }
 };
-
-exports.resetGuestScores = async (req, res) => {
-    try {
-      const guestUser = await User.findOne({ email: "guest@gmail.com" });
-  
-      if (!guestUser) {
-        return res.status(404).json({ message: "Guest user not found" });
-      }
-  
-      guestUser.communication_score = 0;
-      guestUser.aptitude_score = 0;
-      guestUser.technical_score = 0;
-      guestUser.overall_score = 0;
-  
-      await guestUser.save();
-  
-      res.status(200).json({ message: "Guest scores reset successfully" });
-    } catch (error) {
-      res.status(500).json({ message: "Server error", error: error.message });
-    }
-  };
